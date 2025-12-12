4f5bd7a9421620bcf9b686535cae2086dce3663b
Simon Orefici Frederiksen <simof@odense.dk>
2025-12-12 09:16:51 +0100

    Updated activity list fetch

--- kmd_nexus_client/functionality/aktivitetslister.py
+++ kmd_nexus_client/functionality/aktivitetslister.py
@@ -13,54 +13,61 @@ class AktivitetslisteClient:
     def __init__(self, nexus_client: NexusClient):
         self.client = nexus_client
 
-    def hent_aktivitetsliste(self, navn: str, organisation: Optional[dict], medarbejder: Optional[dict], antal_sider: int = 50) -> list[dict] | None:
-        """
-        Fetch activities from Nexus API and return them as a list of dictionaries.
-
-        Returns:
-            Dictionary of activities keyed by activity ID
-        """
+    def hent_aktivitetsliste(
+        self, navn: str, organisation: Optional[dict], medarbejder: Optional[dict], antal_sider: int = 50
+    ) -> list[dict] | None:
         
-        præferencer = self.client.get("preferences").json()        
-        aktivitetsliste = next((item for item in præferencer.get("ACTIVITY_LIST", []) if item.get("name") == navn), None)
+        præferencer = self.client.get("preferences").json()
+        aktivitetsliste = next(
+            (item for item in præferencer.get("ACTIVITY_LIST", []) if item.get("name") == navn),
+            None
+        )
         
         if not aktivitetsliste:
             return None
-        
-        aktivitetsliste = self.client.get(aktivitetsliste["_links"]["self"]["href"]).json()
 
+        aktivitetsliste = self.client.get(aktivitetsliste["_links"]["self"]["href"]).json()
         base_content_url = aktivitetsliste["_links"]["content"]["href"]
 
-        if organisation:
-            content_url = base_content_url + f"&pageSize={antal_sider}&assignmentOrganizationAssignee={organisation['id']}&assignmentProfessionalAssignee=NO_PROFESSIONAL_CRITERIA"
+        # FIXED: correct ordering
+        if organisation and medarbejder:
+            content_url = (base_content_url +
+                        f"&pageSize={antal_sider}"
+                        f"&assignmentOrganizationAssignee={organisation['id']}"
+                        f"&assignmentProfessionalAssignee={medarbejder['id']}")
+        elif organisation:
+            content_url = (base_content_url +
+                        f"&pageSize={antal_sider}"
+                        f"&assignmentOrganizationAssignee={organisation['id']}"
+                        f"&assignmentProfessionalAssignee=NO_PROFESSIONAL_CRITERIA")
         elif medarbejder:
-            content_url = base_content_url + f"&pageSize={antal_sider}&assignmentOrganizationAssignee=ALL_ORGANIZATIONS&assignmentProfessionalAssignee={medarbejder['id']}"
-        elif organisation and medarbejder:
-            content_url = base_content_url + f"&pageSize={antal_sider}&assignmentOrganizationAssignee={organisation['id']}&assignmentProfessionalAssignee={medarbejder['id']}"
+            content_url = (base_content_url +
+                        f"&pageSize={antal_sider}"
+                        f"&assignmentOrganizationAssignee=ALL_ORGANIZATIONS"
+                        f"&assignmentProfessionalAssignee={medarbejder['id']}")
         else:
-            content_url = base_content_url + f"&pageSize={antal_sider}&assignmentOrganizationAssignee=ALL_ORGANIZATIONS&assignmentProfessionalAssignee=NO_PROFESSIONAL_CRITERIA"
+            content_url = (base_content_url +
+                        f"&pageSize={antal_sider}"
+                        f"&assignmentOrganizationAssignee=ALL_ORGANIZATIONS"
+                        f"&assignmentProfessionalAssignee=NO_PROFESSIONAL_CRITERIA")
 
-        activities_dict = []  # Initialize the dictionary to store activities
+        activities_list = []
 
-        try:
-            response = self.client.get(content_url)
-            activities_data = response.json()
-            pages = activities_data["pages"]
-            j = 0            
-            
-            for page in pages:
-                temp_activity = self.client.get(page["_links"]["content"]["href"]).json()
+        # Removed try/except, unless you want custom error handling
+        response = self.client.get(content_url)
+        activities_data = response.json()
 
-                # Handle the case where temp_activity is a list of activities
-                if isinstance(temp_activity, list):
-                    for activity in temp_activity:
-                        if isinstance(activity, dict) and "id" in activity:
-                            activities_dict.append(activity)
+        pages = activities_data.get("pages", [])
 
-                j += 1
+        for i, page in enumerate(pages):
+            if i >= antal_sider:
+                break
 
-                if j >= antal_sider or j == len(pages):
-                    return activities_dict            
-            
-        except Exception:            
-            raise
+            temp_activity = self.client.get(page["_links"]["content"]["href"]).json()
+
+            if isinstance(temp_activity, list):
+                for activity in temp_activity:
+                    if isinstance(activity, dict) and "id" in activity:
+                        activities_list.append(activity)
+
+        return activities_list
